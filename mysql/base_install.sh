51382fcc21e3521fca776487a6facb048a164e3b
mikbill <helgi8@mail.ru>
2016-09-15 12:18:53 +0300

    Update base_install.sh

--- mysql/base_install.sh
+++ mysql/base_install.sh
@@ -4,3 +4,7 @@ mysql -uroot -pROOTPASS mikbill < /var/lib/mysql/mikbill_2_0_6_utf8.sql
 
 rm -f /etc/mikbill/my.cnf
 mv /etc/mikbill/my-mikbill.cnf /etc/mikbill/my.cnf
+
+rm -f var/lib/mysql/ib_logfile0
+rm -f var/lib/mysql/ib_logfile1
+rm -f var/lib/mysql/ibdata1
