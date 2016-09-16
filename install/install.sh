a38ef7a75d394a2ca11c3fd7970251e230c2ab77
mikbill <helgi8@mail.ru>
2016-09-16 22:39:40 +0300

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 mysql_root_passwd="passwd"
 mysql_mikbill_passwd="passwd"
-HOME_DIR=$(cd $(dirname $0)&& pwd)
-source $HOME_DIR/install.lib
+
+dir_install="/tmp/install"
+dir_config="/etc/mikbill"
+dir_base="var/lib/mysql"
+dir_log_nginx="/var/log/nginx"
+dir_mikbill="/var/www/mikbill"
+
+mkdir $dir_install
+wget https://raw.githubusercontent.com/mikbill/distr/master/install/install.lib -O $dir_install/install.lib
+source $dir_install/install.lib
 
 RM_MIKBILL
 #DOCKER_CLEAR
