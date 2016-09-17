2d8df6ab5fb8aeae401a9e7225aca10fe5eeb842
mikbill <helgi8@mail.ru>
2016-09-17 16:06:20 +0300

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -3,20 +3,22 @@ mysql_root_passwd="passwd"
 mysql_mikbill_passwd="passwd"
 
 dir_root="/opt/mikbill"
+dir_mysql="$dir_root/mysql"
+dir_nginx="$dir_root/nginx"
+dir_mikbill="$dir_root/mikbill"
 dir_install="$dir_root/install"
-dir_conf="$dir_root/conf"
-dir_base="$dir_root/base"
-dir_log_nginx="$dir_root/log/nginx"
-dir_www="$dir_root/www"
+#MySQL
+dir_mysql_base="$dir_mysql/base"
+#Nginx
+dir_nginx_vhosts="$dir_nginx/vhosts"
+dir_nginx_logs="$dir_nginx/logs"
+#Mikbill
+dir_mikbill_www="$dir_mikbill/www"
 
-mkdir $dir_install
+mkdir -p $dir_install
 wget https://raw.githubusercontent.com/mikbill/distr/master/install/install.lib -O $dir_install/install.lib
 source $dir_install/install.lib
 
-# Очистка каталогов
-RM_MIKBILL
-# Удаление всех образов и контейнеров в системе
-#DOCKER_CLEAR
 # Создание каталогов
 MKDIR
 # Загрузка всех файлов
