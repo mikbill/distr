cfc8d144d8d990ab356c63d772331f514ef499b2
mikbill <helgi8@mail.ru>
2016-09-19 21:28:25 +0300

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -4,20 +4,7 @@ mysql_mikbill_passwd="passwd"
 # Где будут лежать рабочие конфиги/база
 dir_root="/opt/mikbill"
 # Каталог для загрузки и с которого будет установка
-dir_install="/opt/install"
-# Каталоги контейнеров
-dir_mysql="$dir_root/mysql"
-dir_nginx="$dir_root/nginx"
-dir_mikbill="$dir_root/mikbill"
-dir_radius="$dir_radius/radius"
-
-#MySQL
-dir_mysql_base="$dir_mysql/base"
-#Nginx
-dir_nginx_vhosts="$dir_nginx/vhosts"
-dir_nginx_logs="$dir_nginx/logs"
-#Mikbill
-dir_mikbill_www="$dir_mikbill/www"
+dir_download="/opt/install"
 
 mkdir -p $dir_install
 wget https://raw.githubusercontent.com/mikbill/distr/master/install/install.lib -O $dir_install/install.lib
