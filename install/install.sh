97735127482abf6d0c1e36997fd7dd6d52a93f17
mikbill <helgi8@mail.ru>
2016-09-20 23:10:32 +0300

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -6,7 +6,7 @@ dir_root="/opt/cloud"
 # Каталог для загрузки и с которого будет установка
 dir_download="/opt/install"
 # Загрузка библиотеки
-wget -P $dir_download -O $dir_download/install.lib https://raw.githubusercontent.com/mikbill/distr/master/install/install.lib
+mkdir $dir_download && wget -O $dir_download/install.lib https://raw.githubusercontent.com/mikbill/distr/master/install/install.lib
 source $dir_download/install.lib
 
 # install_docker
