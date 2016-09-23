6520635528e848ea632882ad14da86cf780b2edc
mikbill <helgi8@mail.ru>
2016-09-23 23:16:21 +0300

    Update start_updates.sh

--- control/start_updates.sh
+++ control/start_updates.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-docker exec mikbill rm -f /var/www/mikbill/admin/sys/update/mikbill_current
-docker exec mikbill /var/www/mikbill/admin/sys/update/mikbill_update.sh
+docker exec MIKBILL rm -f /var/www/mikbill/admin/sys/update/mikbill_current
+docker exec MIKBILL /var/www/mikbill/admin/sys/update/mikbill_update.sh
 
-docker restart radius mikbill
+docker restart RADIUS MIKBILL
 
 docker ps
