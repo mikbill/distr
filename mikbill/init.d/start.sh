fb85c52bca2c5a9ea896b85c9967e0d45e2020cc
mikbill <helgi8@mail.ru>
2016-09-26 12:36:07 +0300

    Update start.sh

--- mikbill/init.d/start.sh
+++ mikbill/init.d/start.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 while true
 do
-# Mikbill
 rm -rf /var/run/mikbill.pid
 php /var/www/mikbill/admin/app/lib/mikbill.php kernel -d
-# Cron
-/usr/sbin/crond
-# Php-fpm
-service php_fpm start
-
 sleep 5
 done
