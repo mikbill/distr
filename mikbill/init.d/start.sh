c227c09022088a5c26675782af9a7b6d9f88ca3e
mikbill <helgi8@mail.ru>
2016-09-27 10:45:43 +0300

    Update start.sh

--- mikbill/init.d/start.sh
+++ mikbill/init.d/start.sh
@@ -2,10 +2,10 @@
 /usr/sbin/crond start
 /usr/sbin/php-fpm -D
 
+cd /var/www/mikbill/admin/app/lib
 while true
 do
 rm -rf /var/run/mikbill.pid
-cd /var/www/mikbill/admin/app/lib
 php mikbill.php kernel -d
 sleep 5
 done
